da55a9be59911e3af45eec40130e6a357a4a46fd
X-Niter <rileyman1999@gmail.com>
2025-05-10 16:04:11 -0500

    work done on pages and workflows

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,10 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  images: { unoptimized: true },
+  trailingSlash: true,
+  distDir: 'out'
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
