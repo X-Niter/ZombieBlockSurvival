b6c69a765897ae30fc445907e913b7de3355c3cc
X-Niter <rileyman1999@gmail.com>
2025-05-10 17:26:46 -0500

    working on pages and workflows

---
+++ dashboard/src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="p-10 text-center text-white">
+      <h1 className="text-4xl font-bold text-red-500">404 - Page Not Found</h1>
+      <p className="mt-4 text-gray-400">Sorry, survivor. That page doesn't exist in this outpost.</p>
+    </div>
+  );
+}
