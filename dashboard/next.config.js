788427c13bd1b6350149826fabcedc9ede18aaec
X-Niter <rileyman1999@gmail.com>
2025-05-10 18:21:16 -0500

    working on pages and workflows

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,11 +1,18 @@
-
 /** @type {import('next').NextConfig} */
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const isGithubPages = process.env.NODE_ENV === 'production' && repoName;
+
+/**
+ * Dynamically configure Next.js for GitHub Pages deployments
+ */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  images: { unoptimized: true },
   trailingSlash: true,
-  distDir: 'out'
+  distDir: 'out',
+  images: { unoptimized: true },
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: isGithubPages ? `/${repoName}` : '',
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
